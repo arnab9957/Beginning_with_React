f2ff0008c98f001e51be959426428424b9da6286
Arnab Kumar Dey <arnabkumardey9957@gmail.com>
2025-07-10 23:53:23 +0530

    BugFix

--- CurrencyConverter/src/App.jsx
+++ CurrencyConverter/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-    <h1>hii</h1>
+    <h1 className='text-3xl bg-yellow'>Currency Converter</h1>
     </>
   )
 }
